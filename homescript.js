3548a6581c4076f7c9deaad73f1aa566d767934a
lucasjukr94 <lucasjukr94@gmail.com>
2016-11-28 14:25:35 -0200

    Add files via upload

---
+++ homescript.js
@@ -0,0 +1,10 @@
+function main(){
+  $('.main').hide();
+  $('.main').fadeIn(1000);
+  $('.rede').hide();
+  $('.rede').fadeIn(1000);
+  $('.intro').hide();
+  $('.intro').fadeIn(1000);
+}
+
+$(document).ready(main);
