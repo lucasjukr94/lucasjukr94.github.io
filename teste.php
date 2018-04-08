e936e578efe46b22cbe84ddfefa71d0f29b6e582
lucasjukr94 <lucasjukr94@gmail.com>
2018-04-08 15:21:11 -0300

    AdminLTE removido

---
+++ teste.php
@@ -0,0 +1,55 @@
+<html>
+	<head>
+		<meta charset="utf-8">
+		<meta http-equiv="X-UA-Compatible" content="IE=edge">
+		<title>Portfolio</title>
+		<link rel="stylesheet" type="text/css" href="style/indexcss.css"/>
+	</head>
+	<body>
+	<?php echo 'hello world';?>
+		<div class="navigation">
+			<div class="wrapper">
+				<a href="#" class="link-head">
+					Home
+				</a>
+			</div>
+			<div class="wrapper">
+				<a href="#" class="link-head">
+					Cadastro
+				</a>
+			</div>
+			<div class="wrapper">
+				<a href="http://192.168.0.14:3000/" class="link-head">
+					Contato
+				</a>
+			</div>
+			<div class="wrapper">
+				<a href="#" class="link-head">
+					Portfolio
+				</a>
+				<ul class="navbar-nivel2">
+					<li class="treebody">
+						<a href="#" class="link-head">
+							Hardware
+						</a>
+					</li>
+					<li class="treebody">
+						<a href="#" class="link-head">
+							Software
+						</a>
+					</li>
+					<li class="treebody">
+						<a href="#" class="link-head">
+							Design
+						</a>
+					</li>
+					<li class="treebody">
+						<a href="#" class="link-head">
+							Games
+						</a>
+					</li>
+				</ul>
+			</div>
+		<div>
+	</body>
+</html>
